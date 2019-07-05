63bb719717484ba7de4b6cfc7703ba999c0eae42
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2019-07-05 23:00:36 +0900

    make compatilbe with BASH as well

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,6 +1,11 @@
-export CREATEPJ_HOME="$( cd "$( dirname "$0" )" && pwd )"
+if [[ $SHELL =~ "zsh" ]]; then
+    export CREATEPJ_HOME="$( cd "$( dirname "$0" )" && pwd )"
+elif [[ $SHELL =~ "bash" ]]; then
+    export CREATEPJ_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+else
+    export CREATEPJ_HOME="$( cd "$( dirname "$0" )" && pwd )"
+fi
 
-#!/usr/local/bin/zsh
 function createpj() {
     cd ${CREATEPJ_HOME}
     pjpath=`python createpj.py $1`
